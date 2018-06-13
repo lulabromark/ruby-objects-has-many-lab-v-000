4eacab4784eee54b47e8c08b188baca51f4681a0
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-13 03:45:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,8 @@ class Artist
 
   def add_song(song)
     @@all << song
-    song.artist = self
+    # song.artist = self
+    if song.artist != nil song.artist = self : nil
   end
 
   def add_song_by_name(name)
